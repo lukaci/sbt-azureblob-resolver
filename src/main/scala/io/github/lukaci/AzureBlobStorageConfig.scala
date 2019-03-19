fc8bd76156314a299c0fb8c2edec7a09533a46fd
Luca Coppo <lukaci@gmail.com>
2019-03-19 21:17:36 +0100

    Remove redundant info

--- src/main/scala/io/github/lukaci/AzureBlobStorageConfig.scala
+++ src/main/scala/io/github/lukaci/AzureBlobStorageConfig.scala
@@ -6,8 +6,10 @@ import scala.concurrent.duration._
   * created by lukaci on 16/03/2019.
   */
 
-case class AzureBlobStorageConfig(prefix: String, listBulkSize: Int, longOpDuration: Duration, shortOpDuration: Duration, endpoint: String = "https://{ACCOUNT_NAME}.blob.core.windows.net", scheme: String = "blob")
+case class AzureBlobStorageConfig(listBulkSize: Int, longOpDuration: Duration, shortOpDuration: Duration, endpoint: String = "https://{ACCOUNT_NAME}.blob.core.windows.net", scheme: String = "blob") {
+  def prefix: String = s"$scheme://"
+}
 
 object AzureBlobStorageConfig {
-  val default: AzureBlobStorageConfig = AzureBlobStorageConfig("blob://", 40, 30.minutes, 30.seconds)
+  val default: AzureBlobStorageConfig = AzureBlobStorageConfig(40, 30.minutes, 30.seconds)
 }
